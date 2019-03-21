2650749da017ecbfa0e789d5f3590607c1e7cd90
Mk1499 <m.khaled1499@gmail.com>
2019-03-21 16:24:34 +0200

    Add Group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,19 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all
+    end
+def create
+  @group = Group.new(group_params)
+
+  @group.save
+  redirect_to groups_path
+end
+
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   get 'groups/index'
+  resources :groups
   get 'friend_ship/Friends'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
