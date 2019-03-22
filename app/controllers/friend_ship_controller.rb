ca69437b956fe8895841beb66232b3f751e91967
rahmaZF <rahmaelzefzafy@gmail.com>
2019-03-22 17:56:04 +0200

    add friend_ship#create action

--- app/controllers/friend_ship_controller.rb
+++ app/controllers/friend_ship_controller.rb
@@ -4,5 +4,21 @@ class FriendShipController < ApplicationController
 
   def create
    
+    if User.exists?(email: params[:email])
+      # @creator_id= current_user.id
+      #for testing 
+      
+       @creator_id= 1
+      @myfriend_id=User.find_by_email!(params[:email]).id
+      @friend_ship=FriendShip.new( creator_id: @creator_id,myfriend_id: @myfriend_id)
+      if @friend_ship.save
+        puts "ok"
+      else 
+        puts "no"
+      end
+      @friend_ship.errors.full_messages    
+    end   
+    #  puts params[:email]
+
   end  
 end
