03e2196722431efb7d9f486a27f2368244df25ae
ahmedmagdy <ahmedmagdy2016@gmail.com>
2019-03-24 14:43:45 +0200

    fix credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,13 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      # user.image.url = auth.info.image
+      user.image = auth.info.image
       user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
+      # user.save
       # user.oauth_token = auth.credentials.token
-      # user.first_name = auth.extra.raw_info.first_name
-      # user.last_name = auth.extra.raw_info.last_name
-      # user.email = auth.extra.raw_info.email
+      # user.first_name = auth.auth.info.first_name
+      # user.last_name = auth.auth.info.last_name
       # user.oauth_expires_at = Time.at(auth.credentials.expires_at)
   end
 end
