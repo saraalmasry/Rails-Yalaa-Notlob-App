289d41bd98a93e74ac935cb5ea635353a54e3850
ahmedmagdy <ahmedmagdy2016@gmail.com>
2019-03-24 12:17:47 +0200

    remove unwanted data from social media login

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,11 +18,11 @@ class User < ApplicationRecord
       # user.image.url = auth.info.image
       user.name = auth.info.name
       user.password = Devise.friendly_token[0,20]
-      user.oauth_token = auth.credentials.token
-      user.first_name = auth.extra.raw_info.first_name
-      user.last_name = auth.extra.raw_info.last_name
-      user.email = auth.extra.raw_info.email
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      # user.oauth_token = auth.credentials.token
+      # user.first_name = auth.extra.raw_info.first_name
+      # user.last_name = auth.extra.raw_info.last_name
+      # user.email = auth.extra.raw_info.email
+      # user.oauth_expires_at = Time.at(auth.credentials.expires_at)
   end
 end
 end
